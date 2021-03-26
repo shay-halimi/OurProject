38a37f8dc3feff42d5eab8b7fec2edbb3e73919d
shaked <shaked43210@gmail.com>
2021-03-26 15:12:59 +0300

    ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,23 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    NSString *filePath;
-
-    #ifdef DEBUG
-      NSLog(@"[ --------- Development mode --------- ]");
-      [GMSServices provideAPIKey:@"AIzaSyAU2f_IHncdwreMxEpAWq_35TL9WHkbTxY"];
-      filePath = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist" inDirectory:@"Debug"];
-    #else
-      NSLog(@"[ ********* PRODUCTION MODE ********* ]");
-      [GMSServices provideAPIKey:@"AIzaSyCoYKtXYQumrk-z30YDxIvrFWLHKMedARM"];
-      filePath = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist" inDirectory:@"Release"];
-    #endif
-
-    FIROptions *options = [[FIROptions alloc] initWithContentsOfFile:filePath];
-    [FIRApp configureWithOptions:options];
-
-
+  [GMSServices provideAPIKey:@"AIzaSyAU2f_IHncdwreMxEpAWq_35TL9WHkbTxY"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
